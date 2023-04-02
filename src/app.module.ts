f2639de5270af86b5aa7ac61ba9904d0192fcc1e
sabribv <74263973+sabribv@users.noreply.github.com>
2023-04-02 09:09:23 -0300

    feat(Users): Added a new Users module.
    * Created the users module, controller and service.
    * Installed prettier

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './modules/users/users.module';
+
+const modules = [UsersModule];
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/imonotributo')],
+  imports: [
+    ...modules,
+    MongooseModule.forRoot('mongodb://localhost:27017/imonotributo'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
