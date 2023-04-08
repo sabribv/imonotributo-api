bcbf6a363e58b5ec7ef6a01eb03e937cbe2bd9be
sabribv <74263973+sabribv@users.noreply.github.com>
2023-04-08 19:17:22 -0300

    fix(Management): Fixed management module not uploaded to repo

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,10 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './modules/users/users.module';
 import { CoreModule } from './modules/core/core.module';
-<<<<<<< Updated upstream
-
-const modules = [CoreModule, UsersModule];
-=======
 import { ManagementModule } from './modules/management/management.module';
 import { SharedModule } from './modules/shared/shared.module';
 
-const modules = [CoreModule, ManaggementModule, SharedModule, UsersModule];
->>>>>>> Stashed changes
+const modules = [CoreModule, ManagementModule, SharedModule, UsersModule];
 
 @Module({
   imports: [
---
+++ src/modules/management/management.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('management')
+export class ManagementController {}
