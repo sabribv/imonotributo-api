b4e6fae64b6284af7d35d8f19a86cf9244f7651b
sabribv <74263973+sabribv@users.noreply.github.com>
2023-04-08 18:27:32 -0300

    feat(Shared): Added a new Shared module.
    * Created the shared module, controller and service.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,15 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './modules/users/users.module';
 import { CoreModule } from './modules/core/core.module';
+<<<<<<< Updated upstream
 
 const modules = [CoreModule, UsersModule];
+=======
+import { ManagementModule } from './modules/management/management.module';
+import { SharedModule } from './modules/shared/shared.module';
+
+const modules = [CoreModule, ManaggementModule, SharedModule, UsersModule];
+>>>>>>> Stashed changes
 
 @Module({
   imports: [
