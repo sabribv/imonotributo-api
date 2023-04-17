dfbdf50226f869734c929ea45afe28a1a3d7aecd
Sabrina Buenavida <sabrina.buenavida@sigsensetech.com>
2023-04-17 12:03:32 -0300

    chore: enable CORS

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: ['http://localhost:5000'],
-    methods: ['GET', 'PUT', 'POST', 'OPTIONS'],
-  });
-
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
